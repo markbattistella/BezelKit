930d9ac224865ba8c7a8989626768820e9de2f22
Mark Battistella <mark_battistella+github@hotmail.com>
2024-05-16 17:32:59 +1000

    2024-05-16 - Fix package exclusion

--- Package.swift
+++ Package.swift
@@ -11,12 +11,16 @@ let package = Package(
 	name: "BezelKit",
 	platforms: [.iOS(.v11)],
 	products: [
-		.library(name: "BezelKit", targets: ["BezelKit"])
+		.library(
+            name: "BezelKit",
+            targets: ["BezelKit"]
+        )
 	],
 	dependencies: [],
 	targets: [
 		.target(
 			name: "BezelKit",
+            exclude: ["../../Generator"],
 			resources: [.process("Resources/bezel.min.json")]
 		)
 	]
