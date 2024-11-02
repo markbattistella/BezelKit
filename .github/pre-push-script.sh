a6d44238a78d723994e07cd17b7a83fba7b1f085
Mark Battistella <mark_battistella+github@hotmail.com>
2024-11-02 13:47:25 +1100

    2024-11-02 - Fixes to automation

---
+++ .github/pre-push-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Step 1: Run the JSON to Markdown conversion
+echo "Generating SupportedDeviceList.md from JSON..."
+node ./.github/supported-devices.js
+
+# Step 2: Check if there are changes to commit
+if [[ `git status --porcelain` ]]; then
+  # Add the generated markdown to the staging area
+  git add ./SupportedDeviceList.md
+  echo "SupportedDeviceList.md updated and staged."
+else
+  echo "No changes in SupportedDeviceList.md"
+fi
